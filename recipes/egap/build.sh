bbf7b02793d725253af18beb58fb7cbce5d87f60
Ian M. B. <99409346+iPsychonaut@users.noreply.github.com>
2026-02-28 15:21:35 -0700

    Update build.sh updated to include TUI
    Added EGAP_TUI to path

--- recipes/egap/build.sh
+++ recipes/egap/build.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 set -eux
 
-# Install helper scripts and EGAP entry-point
 mkdir -p "${PREFIX}/bin"
+
 cp -r bin/* "${PREFIX}/bin/"
+
 cp EGAP.py "${PREFIX}/bin/EGAP"
 chmod +x "${PREFIX}/bin/EGAP"
+
+cp EGAP_TUI.py "${PREFIX}/bin/EGAP_TUI"
+chmod +x "${PREFIX}/bin/EGAP_TUI"
